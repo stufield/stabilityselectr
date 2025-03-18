31acb481f0b05c3d1eeb05cc682ff91eb1e232e6
Stu Field <stu.g.field@gmail.com>
2025-03-18 14:34:45 -0600

    Cleaned up progeny clustering figures and examples

--- R/stability-cluster.R
+++ R/stability-cluster.R
@@ -25,32 +25,29 @@
 #'
 #' @examples
 #' stab_clust <- withr::with_seed(999, stability_cluster(progeny_data, k = 3, iter = 500))
-#' table(actual = rep(1:3, each = 50L), predicted = stab_clust$ProbK)
+#' table(actual = rep(1:3L, each = 50L), predicted = stab_clust$ProbK)
 #'
-#' stab_clust <- stab_clust |>
-#' dplyr::mutate(
-#'   sample = dplyr::row_number(),
-#'   pch    = rep(16:18, each = 50),
-#'   pch    = dplyr::case_when(
-#'     sample <= 50 & ProbK != 1L ~ 13L,                 # cluster 1
-#'     sample > 50 & sample <= 100 & ProbK != 2L ~ 13L,  # cluster 2
-#'     sample > 100 & ProbK != 3L ~ 13L,                 # cluster 3
-#'     TRUE ~ pch
-#'   )
-#' )
+#' stab_clust$true_cluster <- rep(1:3L, each = 50L)
 #'
 #' # View the stable clusters
 #' stab_clust
 #'
 #' # View the incorrectly clustered samples (n = 6)
-#' filter(stab_clust, pch == 13)
+#' filter(stab_clust, ProbK != true_cluster)
 #'
 #' # Plot Stability Clusters
+#' cols <- c("#24135F", "#00A499", "#840B55")
 #' withr::with_par(list(mgp = c(2.00, 0.75, 0.0), mar = c(3, 4, 3, 1), mfrow = 1:2L), {
-#'   plot(progeny_data, col = rep(2:4, each = 50), pch = rep(16:18, each = 50),
-#'        cex = 1.75, main = "Simulated 3 Cluster Data")
-#'   plot(progeny_data, col = rep(2:4, each = 50), pch = stab_clust$pch,
-#'        cex = 1.75, main = "Stability Clustering")
+#'   plot(progeny_data,
+#'        col = cols[stab_clust$true_cluster],
+#'        bg  = cols[stab_clust$true_cluster],
+#'        pch = stab_clust$true_cluster + 20,
+#'        lwd = 1, cex = 1.75, main = "Simulated 3 Cluster Data")
+#'   plot(progeny_data,
+#'        col = cols[stab_clust$ProbK],
+#'        bg  = cols[stab_clust$true_cluster],
+#'        pch = stab_clust$true_cluster + 20,
+#'        lwd = 2.5, cex = 1.5, main = "Stability Clustering")
 #' })
 #' @importFrom cluster pam
 #' @importFrom tibble as_tibble
--- inst/progeny_plots/create-plots.R
+++ inst/progeny_plots/create-plots.R
@@ -7,41 +7,43 @@
 # ---------------------------------------------------
 suppressPackageStartupMessages(devtools::load_all(quiet = TRUE))
 
+
 # Progeny Clustering ----
+
 # This can take a while!
 pclust <- withr::with_seed(101,
   progeny_cluster(progeny_data, clust_iter = 2:9L,
-                  reps = 50L, iter = 100L, size = 8, verbose = TRUE)
+                  reps = 25L, iter = 100L, size = 8)
 )
 
-plot(pclust,
-     file = "inst/progeny_plots/progeny_data_output.pdf",
-     height = 5, width = 10)
+p <- plot(pclust)
+
+ggplot2::ggsave(
+  filename = "inst/progeny_plots/progeny_data_output.pdf",
+  plot = p, height = 5, width = 10
+)
+
 
 # Stability Clustering ----
 stab_clust <- withr::with_seed(101, stability_cluster(progeny_data,
                                                       k = 3L, iter = 1000L))
-stab_clust <- dplyr::mutate(
-  stab_clust,
-  sample = dplyr::row_number(),
-  pch    = rep(16:18, each = 50),
-  pch    = dplyr::case_when(
-    sample <= 50 & ProbK != 1L ~ 13L,                # cluster 1
-    sample > 50 & sample <= 100 & ProbK != 2L ~ 13L, # cluster 2
-    sample > 100 & ProbK != 3L ~ 13L,                # cluster 3
-    TRUE ~ pch
-  )
-)
+stab_clust$true_cluster <- rep(1:3L, each = 50L)
 
-cols <- unlist(col_palette[c("purple", "lightgreen", "magenta")])
+cols <- c("#24135F", "#00A499", "#840B55")
 file <- "inst/progeny_plots/progeny_data_stability_scatter.pdf"
 
 withr::with_pdf(file, title = basename(file), width = 12, height = 6, {
   withr::with_par(list(mgp = c(2.00, 0.75, 0.00), mar = c(3, 4, 3, 1),
                        mfrow = 1:2L), {
-    plot(progeny_data, col = rep(cols, each = 50), cex = 1.75,
-         pch = rep(16:18, each = 50), main = "Simulated 3 Cluster Data")
-    plot(progeny_data, col = rep(cols, each = 50), pch = stab_clust$pch,
-         cex = 1.75, main = "Stability Clustering")
+   plot(progeny_data,
+        col = cols[stab_clust$true_cluster],
+        bg  = cols[stab_clust$true_cluster],
+        pch = stab_clust$true_cluster + 20,
+        lwd = 1, cex = 1.75, main = "Simulated 3 Cluster Data")
+   plot(progeny_data,
+        col = cols[stab_clust$ProbK],
+        bg  = cols[stab_clust$true_cluster],
+        pch = stab_clust$true_cluster + 20,
+        lwd = 2.5, cex = 1.5, main = "Stability Clustering")
   })
 })
