438e5488c0f3a112767eda93b45df3482cba4277
Stu Field <stu.g.field@gmail.com>
2026-06-24 23:02:30 -0600

    Major code cleanup
    - simplifying `W` generation calls
      - was being called in multiple places
      - messed with the seed set
      - but unnecessary; call once and pass downstream
    - pulled out generation of `lambda_seq` object
      - now called by a subroutine
      - no more long if/else logic
      - W is passed now to this function
      - but also used downstream in glmnet
        to calculate the betas
      - simplifies seed calls
    - the seed changes necessitated regenerating
      the snapshots for cox
    - removed `...` param from
      `stability_selection()`
    - cleaned up vestigial arguments from error
      handling unit tests; long ago unused
      but now caught because removed the `...`
      param from `stability_selection()`
    - cleaned up examples in docs; regen *.Rd
    - internal `.calcW()` now lives in utils.R
      - the same code was being called in multiple
        places
      - now all lives in one place and called
        from same subroutine
    - calculating the betas near end of call
      now done with a `do.call()` and a `switch()`
      with some modified args for each kernel

---
+++ R/get-lambda-seq.R
@@ -0,0 +1,91 @@
+
+#' Internal to determine the lambda seq object
+#'
+#' @importFrom glmnet glmnet
+#' @noRd
+.get_lambda_seq <- function(x, y, kernel, Pw, standardize, alpha, W,
+                            elastic_alpha, lambda_min_ratio, lambda_pad) {
+
+  if ( kernel == "l1-logistic" ) {
+
+    lambda_seq <- glmnet::glmnet(
+      x, y, family = "binomial", standardize = standardize,
+      lambda.min.ratio = lambda_min_ratio,
+      penalty.factor = W)$lambda
+
+  } else if ( kernel == "multinomial") {
+
+    lambda_seq <- glmnet::glmnet(
+      x, y, family = "multinomial", standardize = standardize,
+      lambda.min.ratio = lambda_min_ratio,
+      penalty.factor = W)$lambda
+
+  } else if ( kernel == "pca.sd" ) {
+    if ( floor(alpha) != alpha ) {
+      stop(
+        "Bad alpha: ", alpha,
+        "\nPlease set to number of principal components to consider.",
+        call. = FALSE
+      )
+    }
+    max_lambda <- 10
+    lambda_seq <- exp(
+      seq(log(max_lambda), log(max_lambda * lambda_min_ratio),
+          length.out = 100L) # hi resolution at low values
+      ) # back to linear space
+
+  } else if ( kernel == "pca.thresh" ) {
+
+    if ( floor(alpha) != alpha ) {
+      stop(
+        "Bad alpha: ", alpha,
+        "\nPlease set to number of principal components to consider.",
+        call. = FALSE
+      )
+    }
+    max_lambda <- 0.5
+    lambda_seq <- exp(
+      seq(log(max_lambda), log(max_lambda * lambda_min_ratio),
+          length.out = 100)
+      )
+
+  } else if ( kernel == "lasso" ) {
+
+    lambda_seq <- glmnet::glmnet(
+      x, y, family = "gaussian", standardize = standardize,
+      lambda.min.ratio = lambda_min_ratio, penalty.factor = W,
+      alpha = elastic_alpha)$lambda
+
+  } else if ( kernel == "ridge" ) {
+    if ( elastic_alpha != 0 ) {
+      stop(
+        "Invalid `elastic_alpha =` argument for 'ridge' kernels (",
+        value(elastic_alpha), "). Please set `elastic_alpha = 0`.",
+        call. = FALSE
+      )
+    }
+    lambda_seq <- glmnet::glmnet(
+      x, y, family = "gaussian", standardize = standardize, alpha = 0,
+      penalty.factor = W)$lambda
+
+  } else if ( kernel == "cox" ) {
+
+    S <- survival::Surv(y[, 1L], y[, 2L])   # come back to this
+    y <- cbind(time = S[, 1L], status = S[, 2L])
+    lambda_seq <- glmnet::glmnet(
+      x, y, nlambda = 100, family = "cox", standardize = standardize,
+      cox.ties = "breslow", # penalty.factor = W, # nolint
+      lambda.min.ratio = lambda_min_ratio)$lambda
+
+  }
+
+  if ( !kernel %in% c("pca.thresh", "pca.sd") ) {
+    # need to pad lambda at the upper end
+    seq_shift <- lambda_seq[1L] / lambda_seq[2L]
+    pad <- rev(seq_len(lambda_pad)) |>  # default 1:20 & invert 20:1
+      vapply(function(.x) lambda_seq[1L] * seq_shift^.x, 0.1)
+    lambda_seq <- c(pad, lambda_seq)
+  }
+
+  lambda_seq
+}
